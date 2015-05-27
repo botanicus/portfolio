e6adef281e9e895157c5552141617076578d3a19
James C Russell <james@101ideas.cz>
2015-05-27 13:31:26 +0100

    Worky, worky!

--- content/app.js
+++ content/app.js
@@ -1,24 +1,22 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.config(function ($locationProvider, $routeProvider, $httpProvider) {
-  window.routes.forEach(function (route) {
-    $routeProvider.when(route.path, route);
-  });
+app.config(function ($locationProvider, $routeProvider) {
+  $routeProvider.
+    when('/', {
+      controller: 'MainController',
+      templateUrl: 'templates/index.html',
+      title: 'TITLE',
+    });
 
   $routeProvider.otherwise({'redirectTo': '/'});
 
   $locationProvider.html5Mode(true);
-
-  // Transmit cookies.
-  // As far as I know, we can't read those cookies
-  // which the server sets in the response, but those
-  // are being set properly, so the login works.
-  $httpProvider.defaults.withCredentials = true;
 });
 
 /* Set up the title. */
-app.run(function ($location, $rootScope, Session) {
-  Session.development = true;
+app.run(function ($location, $rootScope) {
+// app.run(function ($location, $rootScope, Session) {
+  // Session.development = true;
 
   $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
     $rootScope.title = current.$$route ? current.$$route.title : null;
@@ -26,12 +24,12 @@ app.run(function ($location, $rootScope, Session) {
 });
 
 /* Main controller. */
-app.controller('MainController', function ($scope, $window, $location, $http, $modal, Links, Session, currentUser, $cookies) {
-  $scope.$on('$viewContentLoaded', function (event) {
-    // console.log("~ Triggering Google Analytics.");
+app.controller('MainController', function ($scope, $window, $location) {
+  // $scope.$on('$viewContentLoaded', function (event) {
+  //   console.log("~ Triggering Google Analytics.");
 
-    // if (Session.development) {
-    //   $window.ga('send', 'pageview', {page: $location.path()});
-    // };
-  });
+  //   if (Session.development) {
+  //     $window.ga('send', 'pageview', {page: $location.path()});
+  //   };
+  // });
 });
